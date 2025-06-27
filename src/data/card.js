794965436968f93f61d63ee3a66f0854fd6c5e46
degen <degenhinata@gmail.com>
2025-06-27 13:02:05 +0100

    card

--- src/data/card.js
+++ src/data/card.js
@@ -2,25 +2,25 @@
    
        {
         id: '2',
-        image: '../assets/img/barrel.png',
+        image: 'https://demo.themnific.com/chemid/wp-content/uploads/2024/10/008-barrel.png',
         title: 'Industrial Oxgyen Supply',
         whatwedo:'Our industrial oxygen is essential for welding, cutting, and chemical processes, ensuring high purity and consistent performance for your operations.'
     },
        {
         id: '3',
-        image: '../assets/img/bliz.png',
+        image: 'https://demo.themnific.com/chemid/wp-content/uploads/2024/10/002-periodic-table.png',
         title: 'Liquid-nitrogen Solutions',
         whatwedo:'We provide liquid nitrogen for cryogenic cooling, preservation, and various industrial applications, maintaining the highest standards of quality and safety.'
     },
        {
         id: '4',
-        image: '../assets/img/medical.png',
+        image: 'https://demo.themnific.com/chemid/wp-content/uploads/2024/10/004-molecular.png',
         title: ' Medical grade Solutions',
         whatwedo:'Our medical-grade oxygen supports respiratory therapy and critical care, adhering to strict purity and safety standards for patient well-being.'
     },
     {
         id: '5',
-        image: '../assets/img/lab.png',
+        image: 'https://demo.themnific.com/chemid/wp-content/uploads/2024/10/006-laboratory.png',
         title: ' Compressed air solutions',
         whatwedo:'We offer reliable compressed air systems for powering tools and equipment, ensuring consistent performance and safety in various industrial settings.'
     }
